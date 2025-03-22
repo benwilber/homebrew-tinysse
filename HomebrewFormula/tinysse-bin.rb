d59fa3cc36298673ead0f0e11843fb61595a1451
Ben Wilber <benwilber@gmail.com>
2025-03-22 14:00:40 -0400

    Releasing v0.3.1

--- HomebrewFormula/tinysse-bin.rb
+++ HomebrewFormula/tinysse-bin.rb
@@ -1,12 +1,12 @@
 class TinysseBin < Formula
     desc "A programmable server for Server-Sent Events (SSE)."
     homepage "https://github.com/benwilber/tinysse"
-    version "0.3.0"
+    version "0.3.1"
 
     on_macos do
       if Hardware::CPU.intel?
         url "https://github.com/benwilber/tinysse/releases/download/v#{version}/macos-x86_64.tar.gz"
-        sha256 "e5b77562be37df9c8a6b330de75ab21af5b082010b610e8840855d1b520db300"
+        sha256 "a847acf01f690d2dd8f6add1008114e0f6de881904e1efb3ed91b645def5792a"
 
         def install
             bin.install "tinysse"
@@ -15,7 +15,7 @@ class TinysseBin < Formula
 
       if Hardware::CPU.arm?
         url "https://github.com/benwilber/tinysse/releases/download/v#{version}/macos-aarch64.tar.gz"
-        sha256 "a0f1633e79b008d949ffd41b08e87d16a55702c80ea4cca79f3e20b143dece8c"
+        sha256 "5016e122880868f014abbd629b16800b98cd5f49578899295d5c3aebcbf3373c"
 
         def install
             bin.install "tinysse"
